2a1b790fcd184ad660b46e7f19320114139b078f
juicyjames55 <32806507+juicyjames55@users.noreply.github.com>
2018-10-16 15:01:04 -0600

    Update app.js

--- D3_homework/assets/js/app.js
+++ D3_homework/assets/js/app.js
@@ -1,45 +1,120 @@
-// When the browser window is resized, makeResponsive() is called.
-d3.select(window).on("resize", makeResponsive);
+// Set up our chart
+var svgWidth = window.innerWidth;
+var svgHeight = window.innerHeight;
 
-// When the browser loads, makeResponsive() is called.
-makeResponsive();
+var margin = {
+	top: 60,
+	right: 40,
+	bottom: 100,
+	left: 100
+};
 
-// The code for the chart is wrapped inside a function that
-// automatically resizes the chart
-function makeResponsive() {
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
 
-  // if the SVG area isn't empty when the browser loads,
-  // remove it and replace it with a resized version of the chart
-  var svgArea = d3.select("body").select("svg");
+var svg = d3
+	.select('.chart')
+	.append("svg")
+	.attr("width", svgWidth)
+	.attr("height", svgHeight)
+	.append("g")
 
-  // clear svg is not empty
-  if (!svgArea.empty()) {
-    svgArea.remove();
-  }
+// Append group element
+	var chart = svg.append("g")
+	.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  // SVG wrapper dimensions are determined by the current width and
-  // height of the browser window.
-  var svgWidth = window.innerWidth;
-  var svgHeight = window.innerHeight;
+  d3.select("body")
+	 .append("div")
+	 .attr("class", "tooltip")
+	 .style("opacity", 1)
 
-  var margin = {
-    top: 50,
-    bottom: 50,
-    right: 50,
-    left: 50
-  };
+// Import data from the data.csv file, call the function healthData
+  d3.csv("assets/data/data.csv", function(error, healthData) {
+	if (error) throw error;
 
-  var height = svgHeight - margin.top - margin.bottom;
-  var width = svgWidth - margin.left - margin.right;
+ // Parse data
+  healthData.forEach(function(data) {
+	  data.poverty = +data.poverty;
+	  data.healthcare = +data.healthcare;
+  });
 
-  // Append SVG element
-  var svg = d3
-    .select("#scatter")
-    .append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth);
+// Create the scales for the chart
+	var x = d3.scaleLinear().range([0, chartWidth]);
+	var y = d3.scaleLinear().range([chartHeight, 0]);
 
-  // Append group element
-  var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-}
+	var bottomAxis = d3.axisBottom(x);
+	var leftAxis = d3.axisLeft(y);
+
+// Scale the range of the data
+	x.domain([0,d3.max(healthData, function(data){
+		return +data.poverty;
+	})]);
+
+	y.domain([0,d3.max(healthData, function(data){
+		return +data.healthcare;
+	})]);
+
+// Defining tooltip
+	var toolTip = d3.tip()
+		.attr("class", "toolTip")
+		.offset([80,-60])
+		.html(function(data){
+			var state = data.state;
+			var povertyRate = +data.poverty;
+			var healthcare = +data.healthcare;
+			return(state + "<br> Poverty Rate (%): " + povertyRate + "<br> Health Rate (%): " + healthcare)
+		});
+
+	chart.call(toolTip);
+
+// Defining the circles on the chart
+	chart.selectAll("circle")
+		.data(healthData)
+		.enter().append("circle")
+			.attr("cx", function(data, index){
+				console.log(data.poverty);
+				return x(data.poverty);
+			})
+			.attr("cy", function(data, index){
+				console.log(data.healthcare);
+				return y(data.healthcare);
+			})
+			.attr('r', "10")
+			.attr("fill", "blue")
+			.style("opacity", 0.5)
+			.on("click", function(data){
+				toolTip.show(data);
+			});
+
+// Add x-axis
+	chart.append("g")
+		.attr("transform", `translate(0, ${chartHeight})`)
+		.call(bottomAxis);
+
+// Add y-axis
+	chart.append('g')
+		.call(leftAxis);
+
+// Text for y-axis
+	chart.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 0 - margin.left + 40)
+		.attr("x", 0 - (chartHeight))
+		.attr("dy", "1em")
+		.attr("class", "axisText")
+		.style("text-anchor", "margintop")
+		.text("Population in Fair or Poor Health (%)")
+
+// Text for x-axis
+	chart.append("text")
+		.attr("transform", "translate(" + (chartWidth/2) + ", " + (chartHeight + margin.top + 20) + ")")
+		.attr("class", "axisText")
+		.style("text-anchor", "middle")
+		.text("Population Below the Poverty Line (%)");
+
+// Text for title
+ 	chart.append("text")
+		.style("text-anchor", "center")
+		.attr("class", "axisText")
+		.text("Correlation of Health vs. Poverty in USA");
+})
